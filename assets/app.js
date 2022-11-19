5f7c681c4abb8d068d9347c6f9a351134c393754
riveur <riveur.cpy@gmail.com>
2022-11-19 18:37:08 +0100

    add fontawesome

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'bulma/css/bulma.min.css'
 
 // start the Stimulus application
